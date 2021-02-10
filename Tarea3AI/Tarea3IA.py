5fb35d5dfc920c4c7c3e64a7cb4f9a9a00e2571c
Isabel Ortiz Naranjo <35385804+isaonaranjo@users.noreply.github.com>
2021-02-09 18:07:55 -0600

    Add files via upload

---
+++ Tarea3AI/Tarea3IA.py
@@ -0,0 +1,6 @@
+# María Isabel Ortiz Naranjo
+# Inteligencia artificial
+# Tarea: Implementación de un zero sum game AI player usando minimax y α-β pruning
+# Implementar un jugador inteligente usando el algoritmo minimax con α-β pruning:
+
+.
